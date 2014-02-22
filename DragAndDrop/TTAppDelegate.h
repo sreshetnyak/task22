d28ce8cd02ae584bcaa38ec3a566bfe4cfd48337
sreshetnyak <reshetniak.sergey@gmail.com>
2014-02-22 10:39:09 +0200

    Initial commit from local git

---
+++ DragAndDrop/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  DragAndDrop
+//
+//  Created by sergey on 2/22/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DragAndDrop/TTViewController.m
@@ -0,0 +1,40 @@
+//
+//  TTViewController.m
+//  DragAndDrop
+//
+//  Created by sergey on 2/22/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import "TTViewController.h"
+
+@interface TTViewController ()
+
+@end
+
+@implementation TTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+//    Уровень супермен (остальных уровней не будет)
+//    
+//    1. Создайте шахматное поле (8х8), используйте черные сабвьюхи
+//    2. Добавьте балые и красные шашки на черные клетки (используйте начальное расположение в шашках)
+//    3. Реализуйте механизм драг'н'дроп подобно тому, что я сделал в примере, но с условиями:
+//    4. Шашки должны ставать в центр черных клеток.
+//    5. Даже если я отпустил шашку над центром белой клетки - она должна переместиться в центр ближайшей к отпусканию черной клетки.
+//    6. Шашки не могут становиться друг на друга
+//    7. Шашки не могут быть поставлены за пределы поля.
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
